101c8fef616f09ffe38d08accd22cd60f190799e
memoe <memoemukoe@gmail.com>
2025-01-27 11:02:38 +0200

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() { 
   return (
-    <div className="Weather">
+    <div className="Weather mt-5">
       <form>
         <div className="row">
           <div className="col-9">
@@ -11,36 +12,44 @@ export default function Weather() {
               placeholder="Type a city..."
               className="form-control"
               autoFocus="on"
-              autoComplete="off"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary w-100" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
 
-      <h1> Petoria </h1>
+      <h1> Pretoria </h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
       </ul>
-      <div className="row">
-        <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" />
-          6°C | °F
+      <div className="row mt-3"> 
+        <div className="col-6 ">
+          <div className="clearfix">  
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="Mostly Cloudy"
+              className="float-left"
+            />
+            
+            <span className="temperature">6</span>
+            <span className="unit">°C|°F</span>
+            
+             
+        
+          </div>
         </div>
         <div className="col-6">
           <ul>
-            <li>
-              Precipitation: 15%
-            </li>
-            <li>
-              Humidity: 72%
-            </li>
-            <li>
-              Wind: 13 km/h
-            </li>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
           </ul>
         </div>
       </div>
